a213ff59af5993617204e335168a47de7c30eec4
Beto Toro <beto.toro@zemoga.com>
2022-07-18 15:45:04 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Beto Toro on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
